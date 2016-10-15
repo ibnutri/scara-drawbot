ebd128b2482657324f4e04520831171d12118112
triyono, ibnu <ibnu.triyono@gmail.com>
2016-10-15 16:40:51 +0700

    use inert as static files provider

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 'use strict';
 
+const Path = require('path');
 const Hapi = require('hapi');
+const Inert = require('inert');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+        connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
 server.connection({ port: 3000 });
+var io = require('socket.io')(server.listener);
+
+server.register(Inert, () => {});
 
 server.route({
     method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true
+        }
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
 
+// server.route({
+//     method: 'GET',
+//     path: '/{name}',
+//     handler: function (request, reply) {
+//         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+//     }
+// });
 server.start((err) => {
 
     if (err) {
