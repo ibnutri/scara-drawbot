40943ad7838f5c19e1c45cda6dd3d6e30db7e863
triyono, ibnu <ibnu.triyono@gmail.com>
2016-11-05 13:25:58 +0700

    load image data from file, park arm when stopped.

--- scara.js
+++ scara.js
@@ -1,6 +1,7 @@
 var five = require("johnny-five");
 var config = require("./config");
 var kinematic = require("./lib/kinematic.js");
+var fs = require('fs');
 var board = new five.Board({
 	port: config.board.port
 });
@@ -12,9 +13,12 @@ kinematic.start(); // priming the variable
 var elbowOffset = 38;
 var shoulderOffset = 7;
 var animation;
+var animation2;
+var loadedFile = '';
+var startingPoint;
 board.on("ready", function() {
   	config.board.test1();
-  	var startingPoint = kinematic.armMoveTo(5,5);
+  	startingPoint = kinematic.armMoveTo(5,5);
  	elbowServo = new five.Servo({
 	  pin: 6,
 	  // startAt: 45 + elbowOffset
@@ -28,93 +32,8 @@ board.on("ready", function() {
 	  startAt: startingPoint.shoulder
 	});
 
- 	animation = new five.Animation(shoulderServo);
- 	animation2 = new five.Animation(elbowServo);
- 	var love = [
- 		[6,5.5],
-		[6.8,5.5],
-		[7,6],
-		[7.2,5.5],
-		[8,5.5],
-		[8.5,6],
-		[8.5,7],
-		[7.8,7.8],
-		[7,9],
-		[6.2,7.8],
-		[5.5,7],
-		[5.5,6],
-		[6,5.5],
- 	];
- 	var loveResult = [];
- 	var loveX = [];
- 	var loveY = [];
- 	for(var i = 0; i < love.length; i++){
- 		var kinematicResult = kinematic.armMoveTo(love[i][0],love[i][1]);
- 		var kinematicConvert = [kinematicResult.shoulder, kinematicResult.elbow];
- 		loveResult.push(kinematicConvert);
- 		console.log(kinematicConvert);
- 		loveX.push({degrees: kinematicConvert[0]});
- 		loveY.push({degrees: kinematicConvert[1]});
- 	}
- 	console.log(loveX);
-	// kinRet1 = kinematic.armMoveTo(6,5.5);
-	// kinRet2 = kinematic.armMoveTo(6.8,5.5);
-	// kinRet3 = kinematic.armMoveTo(7,6);
-	// kinRet4 = kinematic.armMoveTo(7.2,5.5);
-	// kinRet5 = kinematic.armMoveTo(8,5.5);
+ 	
 
-	// kinRet6 = kinematic.armMoveTo(8.5,6);
-	// kinRet7 = kinematic.armMoveTo(8.5,7);
-	// kinRet8 = kinematic.armMoveTo(7.8,7.8);
-	// kinRet9 = kinematic.armMoveTo(7,9);
-	// kinRet10 = kinematic.armMoveTo(6.2,7.8);
-
-	// kinRet11 = kinematic.armMoveTo(5.5,7);
-	// kinRet12 = kinematic.armMoveTo(5.5,6);
-	// kinRet13 = kinematic.armMoveTo(6,5.5);
-	
-	animation.enqueue({
-		duration:8000,
-		cuePoints: [0, 0.08, 0.16, 0.24, 0.32, 0.40, 0.48, 0.56, 0.64, 0.72, 0.80, 0.96, 1],
-		keyFrames://[
-			// [{degrees: kinRet1.shoulder},{degrees: kinRet2.shoulder},{degrees: kinRet3.shoulder},{degrees: kinRet4.shoulder},{degrees: kinRet5.shoulder},
-			// {degrees: kinRet6.shoulder},{degrees: kinRet7.shoulder},{degrees: kinRet8.shoulder},{degrees: kinRet9.shoulder},
-			// {degrees: kinRet10.shoulder},{degrees: kinRet11.shoulder},{degrees: kinRet12.shoulder},{degrees: kinRet13.shoulder}]
-			// [{degrees: kinRet1.elbow},{degrees: kinRet2.elbow},{degrees: kinRet3.elbow}]
-			// [{degrees: 80},{degrees: 90},{degrees: 100}]
-			// [{},{},{}]
-		//]
-		loveX
-		,
-		metronomic: true,
-		loop: true
-	});
-	animation.stop();
-	animation2.enqueue({
-		duration:8000,
-		cuePoints: [0, 0.08, 0.16, 0.24, 0.32, 0.40, 0.48, 0.56, 0.64, 0.72, 0.80, 0.96, 1],
-		keyFrames://[
-			// [{degrees: kinRet1.shoulder},{degrees: kinRet2.shoulder},{degrees: kinRet3.shoulder}],
-			// [{degrees: kinRet1.elbow},{degrees: kinRet2.elbow},{degrees: kinRet3.elbow},{degrees: kinRet4.elbow},{degrees: kinRet5.elbow},
-			// {degrees: kinRet6.elbow},{degrees: kinRet7.elbow},{degrees: kinRet8.elbow},{degrees: kinRet9.elbow},
-			// {degrees: kinRet10.elbow},{degrees: kinRet11.elbow},{degrees: kinRet12.elbow},{degrees: kinRet13.elbow}]
-			// [{degrees: 80},{degrees: 90},{degrees: 100}]
-			// [{},{},{}]
-		//]
-		loveY
-		,
-		metronomic: true,
-		loop: true
-	});
-	animation2.stop();
-	// animation.enqueue({
-	//     cuePoints: [0, 0.25, 0.75, 1],
-	//     keyFrames: [90, { value: 180, easing: "inQuad" }, { value: 0, easing: "outQuad" }, 90],
-
-	//     duration: 2000
-	// });
- 	//shoulderServo.sweep();
- 	// deviasi 56derajat
  	board.repl.inject({
  	shoulderTo: function(degree){
  		shoulderServo.to(shoulderPosition + degree);
@@ -131,47 +50,13 @@ board.on("ready", function() {
  	makeGrid: makeGrid,
  	startAnim: startAnim,
  	animation: animation,
- 	animation2: animation2
+ 	animation2: animation2,
+ 	loadFile: loadFile
  });
 	this.on("exit", function(){
-		// armMoveTo(5,5);
+		parkArm();
 	});
 });
-function test(){
-	console.log('sparta');
-}
-
-// stdin.on('keypress', function (chunk, key) {
-//   	// if (key && key.ctrl && key.name == 'c') process.exit();
-//   	if(key.name == 'right'){
-//   		console.log('right');	
-//   		if(drawY < 10){
-//   			drawY += 0.5;
-//   			armMoveTo(drawX, drawY);
-//   		}
-//   	}
-//   	if(key.name == 'left'){
-//   		console.log('left');	
-//   		if(drawY > 5){
-//   			drawY -= 0.5;
-//   			armMoveTo(drawX, drawY);
-//   		}
-//   	}
-//   	if(key.name == 'up'){
-//   		console.log('up');	
-//   		if(drawX > 5){
-//   			drawX -= 0.5;
-//   			armMoveTo(drawX, drawY);
-//   		}
-//   	}
-//   	if(key.name == 'down'){
-//   		console.log('down');	
-//   		if(drawX < 10){
-//   			drawX += 0.5;
-//   			armMoveTo(drawX, drawY);
-//   		}
-//   	}
-// });
 
 
 
@@ -230,6 +115,56 @@ function doSetTimeout(i,currentX,currentY) {
   	}, i);
 }
 function startAnim(){
+	if(loadedFile == ''){
+		console.log('No file loaded, please load file first');
+		return;
+	}
 	animation.play();
 	animation2.play();
- }
+}
+function loadFile(filename){
+	if(filename == undefined){
+		filename = 'sample-image/heart.json'
+	}
+	var obj;
+	fs.readFile(filename, 'utf8', function (err, data) {
+		if (err) throw err;
+		obj = JSON.parse(data);
+		console.log('loaded '+obj.name);
+		loadedFile = obj.name;
+		var love = obj.path;
+		animation = new five.Animation(shoulderServo);
+	 	animation2 = new five.Animation(elbowServo);
+	 	var loveResult = [];
+	 	var loveX = [];
+	 	var loveY = [];
+	 	for(var i = 0; i < love.length; i++){
+	 		var kinematicResult = kinematic.armMoveTo(love[i][0],love[i][1]);
+	 		var kinematicConvert = [kinematicResult.shoulder, kinematicResult.elbow];
+	 		loveResult.push(kinematicConvert);
+	 		loveX.push({degrees: kinematicConvert[0]});
+	 		loveY.push({degrees: kinematicConvert[1]});
+	 	}
+		
+		animation.enqueue({
+			duration:8000,
+			cuePoints: [0, 0.08, 0.16, 0.24, 0.32, 0.40, 0.48, 0.56, 0.64, 0.72, 0.80, 0.96, 1],
+			keyFrames: loveX,
+			metronomic: true,
+			loop: true
+		});
+		animation.stop();
+		animation2.enqueue({
+			duration:8000,
+			cuePoints: [0, 0.08, 0.16, 0.24, 0.32, 0.40, 0.48, 0.56, 0.64, 0.72, 0.80, 0.96, 1],
+			keyFrames:loveY,
+			metronomic: true,
+			loop: true
+		});
+		animation2.stop();
+	});
+}
+function parkArm(){
+	shoulderServo.to(startingPoint.shoulder);
+	elbowServo.to(startingPoint.elbow);
+}
